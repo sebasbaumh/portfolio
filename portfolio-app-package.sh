08b56f49f275cfd19e2b0d19d6d1a993dc47ef9a
Sebastian Baumhekel <sebastian.baumhekel@gmail.com>
2025-05-07 08:50:39 +0200

    adjust build script to working one on Mac.

--- portfolio-app-package.sh
+++ portfolio-app-package.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-export MAVEN_OPTS="-Xmx4g"
-mvn --update-snapshots -D eclipse.p2.mirrors=false --file=portfolio-app/pom.xml clean verify
+#export MAVEN_OPTS="-Xmx4g"
+#mvn --update-snapshots -D eclipse.p2.mirrors=false --file=portfolio-app/pom.xml clean verify
+rm -rf */target                                                           1 ↵
+#rm -rf */**/target
+mvn -f portfolio-app/pom.xml -Denforcer.skip=true clean verify
