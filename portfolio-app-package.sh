aab0ad931c3487370685d3042899cc04a193b341
Sebastian Baumhekel <sebastian.baumhekel@gmail.com>
2025-06-10 11:13:05 +0200

    Add skip tests.

--- portfolio-app-package.sh
+++ portfolio-app-package.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 #export MAVEN_OPTS="-Xmx4g"
 #mvn --update-snapshots -D eclipse.p2.mirrors=false --file=portfolio-app/pom.xml clean verify
+# clean up old builds
 rm -rf */target                                                           1 ↵
-#rm -rf */**/target
+# build
 mvn -f portfolio-app/pom.xml -Denforcer.skip=true clean verify
+# skip tests
+#mvn -f portfolio-app/pom.xml -Denforcer.skip=true -DskipTests clean verify
